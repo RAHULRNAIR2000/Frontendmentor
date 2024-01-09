09330a93949fde5c329cde59f051f29bcc083316
RAHULRNAIR2000 <rahulrnairsreemangalam@gmail.com>
2024-01-09 16:32:01 +0530

    training inserted

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import card from '../src/images/image-qr-code.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="card">
+          <img src={require('../src/images/image-qr-code.png')} className="logo" alt="QR Code" />
+          <p className = "para">
+         improve your front end skills by building projects
+         </p>
+         <p className = "para1">
+          Scan the QR vode to visit Frontend Mentor and take your coding skills to the next level
+         </p>
+        </div>       
+       
       </header>
     </div>
   );
